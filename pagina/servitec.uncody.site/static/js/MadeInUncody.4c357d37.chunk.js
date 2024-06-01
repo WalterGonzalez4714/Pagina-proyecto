1ce51eacb37a244481413243c735ca4583263a6b
WalterGonzalez4714 <waltergonzalez.ortega@gmail.com>
2024-05-31 20:52:03 -0600

    Add files via upload

---
+++ pagina/servitec.uncody.site/static/js/MadeInUncody.4c357d37.chunk.js
@@ -0,0 +1,18 @@
+"use strict";
+(self.__LOADABLE_LOADED_CHUNKS__ = self.__LOADABLE_LOADED_CHUNKS__ || []).push([[6025], {
+    7410: function(e, t, p) {
+        p.r(t);
+        var a = p(7294),
+            o = p(822),
+            n = function() {
+                return o.a.generateStyle({
+                    ".uncodyBadge": {
+                        display: "none" // Oculta el contenedor
+                    }
+                }), a.createElement("div", {
+                    className: "fixed bottom-0 py-2 flex justify-center z-[0] w-full"
+                }, )
+            };
+        t.default = n
+    }
+}]);
